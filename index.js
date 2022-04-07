82c5b14e860f7e2a0d8bb4669213b8c7f3b211fb
matsegard <matsegard@outlook.com>
2022-04-07 10:23:06 +0200

    Added delete user logic

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
+app.use(express.json())
+
+// app.use("/", express.static("public"))
+
+
+const users = [
+    {
+        name: "Tony",
+        age: 44,
+        occupation: "Police",
+        id: 1
+    }, 
+    {
+        name: "Anna",
+        age: 62,
+        occupation: "Banker",
+        id: 2
+    },
+    {
+        name: "Erik",
+        age: 33,
+        occupation: "Doctor",
+        id: 3
+    },
+    {
+        name: "Emil",
+        age: 25,
+        occupation: "Carpenter",
+        id: 4
+    }
+]
+
+
+app.get("/api/users", (req, res) => { 
+    if(users.length === 0){
+res.status(404).send('Users not found')
+    } else {
+        res.json(users)
+    }
 })
 
-app.listen(port, () => console.log("App is listening on port: " + port))
+app.post("/api/users", (req, res) => {
+    users.push(req.body)
+    res.status(201).send(users)
+})
+
+app.put("api/users/:id", (req, res) => {
+
+})
+
+app.delete("/api/users/:id", function(req, res) {
+    const itemIndex = users.findIndex(({ id }) => id == req.params.id);
+    if (itemIndex >= 0) {
+      users.splice(itemIndex, 1);
+    }
+    res.send('User deleted')
+});
+  
+
+app.listen(port, () => { console.log("App is now running on port: " + port) })
